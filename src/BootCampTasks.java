fad5e9409b1969f4f4748a4ad51506801c3dde51
Hayoto <bluelink1396@gmail.com>
2015-11-18 20:00:11 -0500

    Bootcamp

---
+++ src/BootCampTasks.java
@@ -0,0 +1,17 @@
+
+public class BootCampTasks {
+	public static int smallest(int[] arr) {
+		int min = 0;
+		for (int i = 0; i < arr.length; ++i) {
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+	public static int populateArray(int left, int right, int count, int[] arr){
+		return 0;
+		
+	}
+	
+}
