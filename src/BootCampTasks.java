f4fa61e9ac0276ffa58511e978d83075b978c3a6
Hayoto <bluelink1396@gmail.com>
2015-11-18 20:01:34 -0500

    Bootcamp2

--- src/BootCampTasks.java
+++ src/BootCampTasks.java
@@ -1,17 +1,60 @@
 
 public class BootCampTasks {
+
 	public static int smallest(int[] arr) {
-		int min = 0;
+		int min = arr[0];
 		for (int i = 0; i < arr.length; ++i) {
-			if (min > arr[i]) {
+			if (arr[i] < min) {
 				min = arr[i];
 			}
 		}
 		return min;
 	}
-	public static int populateArray(int left, int right, int count, int[] arr){
-		return 0;
-		
+
+	public static int[] populateArray(int left, int right, int count) {
+		int[] arr = new int[count];
+		for (int i = left; i < right; ++i) {
+			arr[i] = (int) (Math.random() * 10);
+		}
+		return arr;
+	}
+
+	public static double[] populateArray(double left, double right, int count) {
+		double[] arr = new double[count];
+		for (int i = (int) left; i < right; ++i) {
+			arr[i] = Math.random() * 10;
+		}
+		return arr;
 	}
 	
+	public static int[] grow(int[] arr){
+		int[] tmp = new int[2 * arr.length];
+		for (int i = 0; i < arr.length; ++i){
+			tmp[i] = arr[i];
+		}
+		return tmp;
+	}
+	
+	public static int[] shrink(int[] arr){
+		int[] tmp = new int[arr.length / 2];
+		for (int i = 0; i < arr.length; ++i){
+			tmp[i] = arr[i];
+		}
+		return tmp;
+	}
+	public static double[] grow(double[] arr){
+		double[] tmp = new double[2 * arr.length];
+		for (int i = 0; i < arr.length; ++i){
+			tmp[i] = arr[i];
+		}
+		return tmp;
+	}
+	
+	public static double[] shrink(double[] arr){
+		double[] tmp = new double[arr.length / 2];
+		for (int i = 0; i < arr.length; ++i){
+			tmp[i] = arr[i];
+		}
+		return tmp;
+	}
 }
